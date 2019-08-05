1183625136a079dc6af7410570b48d9ec7bf498c
ryuichi1208 <ryucrosskey@gmail.com>
2019-08-05 22:36:50 +0900

    first commit

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def callback():
     signature = request.headers['X-Line-Signature']
 
     body = request.get_data(as_text=True)
+    print(body)
     app.logger.info("Request body: " + body)
 
     try:
